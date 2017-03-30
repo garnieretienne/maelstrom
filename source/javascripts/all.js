eb470149e824a52c82d78226417a86c32c4ef2eb
Etienne Garnier <garnier.etienne@gmail.com>
2017-03-30 17:17:58 +0200

    prepare the website for the maelstrom #4

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,12 @@
 //= require_tree .
+
+var easteregg = document.getElementById('easteregg');
+var smile = document.querySelector("#easteregg img");
+
+easteregg.addEventListener("mouseenter", function(e) {
+  smile.setAttribute('src', 'images/smile2.jpg');
+});
+
+easteregg.addEventListener("mouseleave", function(e) {
+  smile.setAttribute('src', 'images/smile.jpg');
+});
