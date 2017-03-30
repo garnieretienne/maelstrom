bca9953c37cc8762bb6b92e84f035e0d3bffb214
Etienne Garnier <garnier.etienne@gmail.com>
2017-03-30 18:51:35 +0200

    disable easter egg and link to subscription for now

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,10 +3,10 @@
 var easteregg = document.getElementById('easteregg');
 var smile = document.querySelector("#easteregg img");
 
-easteregg.addEventListener("mouseenter", function(e) {
-  smile.setAttribute('src', 'images/smile2.jpg');
-});
-
-easteregg.addEventListener("mouseleave", function(e) {
-  smile.setAttribute('src', 'images/smile.jpg');
-});
+// easteregg.addEventListener("mouseenter", function(e) {
+//   smile.setAttribute('src', 'images/smile2.jpg');
+// });
+//
+// easteregg.addEventListener("mouseleave", function(e) {
+//   smile.setAttribute('src', 'images/smile.jpg');
+// });
